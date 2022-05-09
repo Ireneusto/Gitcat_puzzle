f371ef9a880282e888db25e165747c320264160b
Ireneusto <ireneusto@gmail.com>
2022-05-09 18:37:45 +0200

    Buttons added to script

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let test = 5
-console.log(5)
+let playButton = document.getElementById("play-btn")
+let tileButton1 = document.getElementById("tile-1")
+let tileButton2 = document.getElementById("tile-2")
+let tileButton3 = document.getElementById("tile-3")
+let tileButton4 = document.getElementById("tile-4")
+//do it with array
+let number = 0
+
+playButton.addEventListener("click", function() {
+    shuffleTiles()
+})
+
+tileButton1.addEventListener("click", function() {
+    tileButton1.textContent = "testing"
+})
+
+function shuffleTiles() {
+    randomNumber = Math.floor( Math.random() * 4) + 1
+    console.log(randomNumber)
+}
