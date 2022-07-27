8fd13e2af8958eada668c4cb2b27a35c6c3f7ba5
Ireneusto <ireneusto@gmail.com>
2022-07-27 11:54:09 +0200

    Array added to script

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
+const catArray = [
+    {
+        name: 'tile1',
+        img: 'img/tile1.jpg',
+    },
+    {
+        name: 'tile2',
+        img: 'img/tile2.jpg',
+    },
+    {
+        name: 'tile3',
+        img: 'img/tile3.jpg',
+    },
+    {
+        name: 'tile4',
+        img: 'img/tile4.jpg',
+    }
+]
+
 let playButton = document.getElementById("play-btn")
-let tileButton1 = document.getElementById("tile-1")
-let tileButton2 = document.getElementById("tile-2")
-let tileButton3 = document.getElementById("tile-3")
-let tileButton4 = document.getElementById("tile-4")
-//do it with array
-let number = 0
-
 playButton.addEventListener("click", function() {
-    shuffleTiles()
+    window.location.reload()
 })
 
-tileButton1.addEventListener("click", function() {
-    tileButton1.textContent = "testing"
-})
 
-function shuffleTiles() {
-    randomNumber = Math.floor( Math.random() * 4) + 1
-    console.log(randomNumber)
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard() {
+    for (let i = 0; i < catArray.length; i++) {
+        const card = document.createElement('img')
+        let randomNumber = 0
+        card.setAttribute('src', 'img/blank-card.jpg')
+        card.setAttribute('data-id', randomNumber)
+        card.addEventListener('click', flipCard)
+        gridDisplay.appendChild(card)
+    }
+}
+createBoard()
+
+function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardId = Math.floor(Math.random() * 4)
+    console.log('clicked!', cardId)
+    this.setAttribute('src', catArray[cardId].img)
 }
